698a552d1def99f58646dccd0cfc7b67d2b7043e
Miranaq <113391061+Miranaq@users.noreply.github.com>
2023-05-22 18:21:26 +0300

    Create 44.java

---
+++ 44/44.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static String sum(String num1, String num2) {
+        // Преобразуем строки в числа
+        int n1 = Integer.parseInt(num1);
+        int n2 = Integer.parseInt(num2);
+
+        // Складываем числа
+        int sum = n1 + n2;
+
+        // Преобразуем результат обратно в строку и возвращаем
+        return String.valueOf(sum);
+    }
+}
