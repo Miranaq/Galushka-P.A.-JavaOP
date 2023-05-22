29146c0fb9894bff9d1d768a9aca01430746f0da
Miranaq <113391061+Miranaq@users.noreply.github.com>
2023-05-22 18:19:57 +0300

    Create 42.java

---
+++ 42/42.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static int countIntervals(int[][] intervals) {
+        int count = 0;
+        for (int i = 0; i < intervals.length - 1; i++) {
+            int end = intervals[i][1];
+            int startNext = intervals[i + 1][0];
+            count += startNext - end - 1;
+        }
+        return count;
+    }
+}
