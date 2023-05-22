a596a3e32229801430818f4ea447a0b0cd683f7f
Miranaq <113391061+Miranaq@users.noreply.github.com>
2023-05-22 18:03:14 +0300

    Create 35.java

---
+++ 35/35.java
@@ -0,0 +1,27 @@
+package thirdHomework.first.decipheringTheCaesarCipher;
+
+import secondHomework.parenthesesFunction.caesarCipher.Caesar;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner1 = new Scanner(System.in);
+        String userText = "";
+        System.out.println("Введите шаг");
+        int step = scanner1.nextInt();
+        String separate = File.separator;
+        File file = new File("/Users/User/PycharmProjects/Galushka-Java/Text");
+        Scanner scanner2 = new Scanner(file);
+        while (scanner2.hasNextLine()) {
+            userText += scanner2.nextLine() + "\n";
+        }
+        Caesar caesar = new Caesar();
+        String cipher = caesar.encryption(userText, step);
+        System.out.println(userText);
+        System.out.println("Зашифрованное слово: " + caesar.encryption(userText, step));
+        System.out.println("Расшифрованно: " + caesar.decipheringTheCaesarCipher(cipher, step));
+    }
+}
